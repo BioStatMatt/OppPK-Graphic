a7f7aa5fb645fa56c3e641b989b635946b18636f
Hannah Weeks <hannahlweeks@gmail.com>
2016-12-06 12:46:46 -0600

    Added table for common dosing pattern, updated plot to accomodate

--- shiny.R
+++ shiny.R
@@ -30,11 +30,27 @@ ui <- fluidPage(
       selectInput("nDoseAdmin", "Number of Doses Administered", 
                   choices = 0:10, # I'm not sure what a reasonable upper bound would be here
                   selected = 0),
-      # Set up area for rhandsontable output
+      
+      # Section for dosing information
+      helpText("Dosing Schedule"),
+      # Set up checkbox and table for common dosing pattern
+      checkboxInput("common", "Common Dosing Pattern"),
+      conditionalPanel(
+        condition = "input.common == true",
+        # Inputs needed for table
+        numericInput("freq", "Dose Frequency (h)", value = 8, min = 0),
+        numericInput("duration", "Infusion Duration (h)", value = .5, min = 0),
+        numericInput("infRate", "Infusion Rate (g/h)", value = 6, min = 0),
+        # Actual table
+        actionButton("goT", "Update Table"),
+        tableOutput("commonPattern")
+      ),
+      rHandsontableOutput("dosing"),  
+      
+      # Section for sample information
       helpText("Sample Information"),
       rHandsontableOutput("sample"),
-      helpText("Dosing Schedule"),
-      rHandsontableOutput("dosing"),
+      
       # Button for updating plot
       actionButton("go", "Update Plot")
     ),
@@ -78,8 +94,10 @@ server <- function(input, output) {
   })
   
   output$dosing <- renderRHandsontable({
-    doseDat()
+    if(1 - input$common){doseDat()}
   })
+  
+  ##########################################################
   
   # These functions allow me to grab the data from rhandsontable to use in plotting
   sampTable <- eventReactive(input$go, {
@@ -91,48 +109,90 @@ server <- function(input, output) {
     return(live_data)
   })
   
+  ##########################################################
+  # Make table if a common dosing pattern was used
+  comTab <- eventReactive(input$goT, {
+    if(input$common){
+      nDose <- as.numeric(input$nDoseAdmin)
+      begin <- seq(0, (nDose - 1) * input$freq, by = input$freq)
+      end <- begin + input$duration
+      kR <- rep(input$infRate, nDose)
+      comPat <- data.frame(begin, end, kR)      
+    }else{
+      comPat <- NULL
+    }
+    return(comPat)
+  })
   
+  output$commonPattern <- renderTable({
+    comTab()
+  })
+  
+  ##########################################################
   
   # Create the plot
   output$plot <- renderPlot({
     # Won't produce plot unless both sample information and dosing schedule has been provided
     if(input$nMeas > 0 & input$nDoseAdmin > 0){
       
+      # SAMPLE INFORMATION
       # Get data from rhandsontable
       datHot <- sampTable()
-      ivtHot <- doseTable()
-      
       # Required for compatibility with functions from Bayes.R
       names(datHot) <- c("time_h", "conc_mg_dl")
-      names(ivtHot) <- c("begin", "end", "k_R")
-      ivtHot <- apply(ivtHot, MARGIN = 1, function(x) list(begin = x[1], end = x[2], k_R = x[3]))
+      
+      # DOSING INFORMATION
+      if(input$common){
+        # Common dosing pattern
+        ivtData <- comTab()
+        
+        # Required for compatibility with functions from Bayes.R
+        names(ivtData) <- c("begin", "end", "k_R")
+        ivtData <- apply(ivtData, MARGIN = 1, function(x) list(begin = x[1], end = x[2], k_R = x[3]))  
+      }else{ # Common dosing pattern not specified
+        # Get data from rhandsontable
+        ivtHot <- doseTable()
+        
+        # Required for compatibility with functions from Bayes.R
+        names(ivtHot) <- c("begin", "end", "k_R")
+        ivtHot <- apply(ivtHot, MARGIN = 1, function(x) list(begin = x[1], end = x[2], k_R = x[3])) 
+        ivtData <- ivtHot
+      }
       
       # Functions from Bayes.R
       # To try default example, use:
       #    dat = data.frame(time_h = c(1,4,40), conc_mg_dl = c(82.7,80.4,60))
       #    ivt = ivt_d
-      est <- optim(lpr_mean_d, log_posterior, ivt=ivtHot,
+      est <- optim(lpr_mean_d, log_posterior, ivt=ivtData,
                    dat=datHot, control = list(fnscale=-1), hessian=TRUE)
-      plot_post_conc(est, ivtHot, datHot)
+      plot_post_conc(est, ivtData, datHot)
       
       
     }else if(input$nMeas == 0){
       if(input$nDoseAdmin > 0){
         dat <- data.frame("empty" = numeric(0))
         
-        ivt_d <- list(list(begin=0.0, end=0.5, k_R=6),
-                      list(begin=8.0, end=8.5, k_R=6),
-                      list(begin=16.0, end=16.5, k_R=6),
-                      list(begin=24.0, end=24.5, k_R=6),
-                      list(begin=32.0, end=32.5, k_R=6))
+        # Get dosing information
+        if(input$common){
+          # Common dosing pattern
+          ivtData <- comTab()
+          
+          # Required for compatibility with functions from Bayes.R
+          names(ivtData) <- c("begin", "end", "k_R")
+          ivtData <- apply(ivtData, MARGIN = 1, function(x) list(begin = x[1], end = x[2], k_R = x[3]))  
+        }else{ # Common dosing pattern not specified
+          # Get data from rhandsontable
+          ivtHot <- doseTable()
+          
+          # Required for compatibility with functions from Bayes.R
+          names(ivtHot) <- c("begin", "end", "k_R")
+          ivtHot <- apply(ivtHot, MARGIN = 1, function(x) list(begin = x[1], end = x[2], k_R = x[3])) 
+          ivtData <- ivtHot
+        }
         
-        ivtHot <- doseTable()
-        names(ivtHot) <- c("begin", "end", "k_R")
-        ivtHot <- apply(ivtHot, MARGIN = 1, function(x) list(begin = x[1], end = x[2], k_R = x[3]))
-        
-        est <- optim(lpr_mean_d, log_posterior, ivt=ivtHot,
+        est <- optim(lpr_mean_d, log_posterior, ivt=ivtData,
                      dat=dat, control = list(fnscale=-1), hessian=TRUE)
-        plot_post_conc(est, ivtHot, dat)
+        plot_post_conc(est, ivtData, dat)
       }
     }
   })
